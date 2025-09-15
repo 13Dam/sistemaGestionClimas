3e6178d69e26a92539a6108040a5fa3ca3ba78f8
NexRide <facugrossovittore@gmail.com>
2025-09-15 18:43:46 -0300

    Corrección tiempo

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -35,8 +35,21 @@ async function actualizarPanelCiudad(cityKey, isChecked) {
     
     if (data.length > 0) {
       const latest = data[0]; // último registro
+      var date = new Date(latest.timestamp * 1000);
+
+      // Hours part from the timestamp
+      var hours = date.getHours();
+
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
       panel.querySelector("p.display-6").textContent = `${latest.temperature} °C`;
-      panel.querySelector("p.small").textContent = `⏰ ${latest.timestamp}`;
+      panel.querySelector("p.small").textContent = `⏰ ${latest.formattedTime}`;
     } else {
       panel.querySelector("p.display-6").textContent = "- °C";
       panel.querySelector("p.small").textContent = "⏰ -";
